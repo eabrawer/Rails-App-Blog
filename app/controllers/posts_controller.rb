940c585998d4f8f184525714e5bba7b164bf46a7
Eric Brawer <ericbrawer@gmail.com>
2013-09-08 16:01:26 -0400

    Edit, Update, Delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,11 +21,22 @@ class PostsController < ApplicationController
 	end
 
 	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
+		if @picture.update_attribues(params.require(:post).permit(:title, :content))
+			redirect_to posts_url
+		else
+			render :edit
+		end 
+
 	end 
 
 	def delete
+		@post = Post.find(params[:id])
+    	@post.destroy
+    	redirect_to posts_url
 	end 
 end
